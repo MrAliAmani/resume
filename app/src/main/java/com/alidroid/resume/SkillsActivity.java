0cb4c96768c96added522d1100648803dcaf4312
MrAliAmani <47820469+MrAliAmani@users.noreply.github.com>
2021-06-30 17:56:57 +0430

    Initial commit

---
+++ app/src/main/java/com/alidroid/resume/SkillsActivity.java
@@ -0,0 +1,14 @@
+package com.alidroid.resume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SkillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skills);
+    }
+}
